e780f837b932c0ed1cffe4c21f298032b6457c0d
Charles Hoffman <chuck.hoffman@banno.com>
2018-04-08 10:22:11 -0500

    refactor display

--- conway.rb
+++ conway.rb
@@ -8,24 +8,28 @@ coordinates = []
 ARGF.each_line do |line|
   coordinates << line.split(',').map(&:to_i)
 end
-
 world = Conway::World.from_coordinate_list(coordinates)
 generation = 0
 
+def display(world, rows, columns, generation)
+  display = Conway::AsciiDisplay.render(world, rows, columns)
+  puts %x{clear}
+  puts display
+  puts "generation #{generation}"
+end
+
+rows, columns = TermInfo.screen_size
 begin
   loop do
-    rows, columns = TermInfo.screen_size
-    display = Conway::AsciiDisplay.render(world, rows-2, columns-1)
-    puts %x{clear}
-    puts display
-    puts "generation #{generation}"
+    # uncomment to allow resizing window
+    #rows, columns = TermInfo.screen_size
+    display(world, rows-2, columns-1, generation)
     sleep(0.1)
     generation += 1
     world = world.tick
   end
 rescue SystemExit, Interrupt
-  puts %x{clear}
-  puts display
+  display(world, rows-2, columns-1, generation)
   puts world.cells.map(&:location).sort.map(&:to_s).join("\n")
 end
 
