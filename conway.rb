cbea5a0e4eb09ab4396a7392f28d77c32091998b
Charles Hoffman <chuck.hoffman@banno.com>
2018-04-11 11:30:31 -0500

    move the display function

--- conway.rb
+++ conway.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-require 'terminfo'
 require './lib/world.rb'
 require './lib/ascii_display.rb'
 
@@ -9,25 +8,17 @@ ARGF.each_line do |line|
   coordinates << line.split(',').map(&:to_i)
 end
 
-def display(world, generation)
-  rows, columns = TermInfo.screen_size
-  display = Conway::AsciiDisplay.render(world, rows - 2, columns)
-  puts `%x(clear)`
-  puts display
-  puts "generation: #{generation} cell count: #{world.cell_count}"
-end
-
 world = Conway::World.from_coordinate_list(coordinates)
 count_log = []
 begin
   loop do
-    display(world, count_log.count)
+    Conway::AsciiDisplay.display(world, count_log.count)
     sleep(0.1)
     count_log << world.cell_count
     world = world.tick
   end
 rescue SystemExit, Interrupt
-  display(world, count_log.count)
+  Conway::AsciiDisplay.display(world, count_log.count)
   puts '---'
   puts 'cells at time interrupted:'
   puts world.cells.map(&:location).sort.map(&:to_s).join("\n")
--- lib/ascii_display.rb
+++ lib/ascii_display.rb
@@ -1,3 +1,5 @@
+require 'terminfo'
+
 module Conway
   # turn a Conway::World into a string suitable for displaying on terminal
   module AsciiDisplay
@@ -15,6 +17,14 @@ module Conway
           end
         }.join
       }.join("\n")
+    end
+
+    def self.display(world, generation)
+      rows, columns = TermInfo.screen_size
+      rendered = Conway::AsciiDisplay.render(world, rows - 2, columns)
+      puts `%x(clear)`
+      puts rendered
+      puts "generation: #{generation} cell count: #{world.cell_count}"
     end
   end
 end
